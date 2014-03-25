e17c6ed8fec9d4e5341234fb8188b8177bacf800
David Pohan <pohandav@gmail.com>
2014-03-25 19:22:07 +0100

    language switching, more functionality, ready to deploy

--- js/helper.js
+++ js/helper.js
@@ -5,22 +5,86 @@ var loaded = false;
 
 var wordSize;
 
-$.ajax({
-	type: 'get',
-	url: 'words/cs.txt',
-	success: function(data) {
-		words = data.toLowerCase().split('\n');
-		debug('words: ' + words);
-		loaded = true;
+var defaultLang = "cs";
+var path = "";
+
+var defaultLetters = {
+	cs: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','ě','š','č','ř','ž','ý','á','í','é','ú','ů'], 
+	en: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+};
+
+var texts = {
+	cs: {
+		wordSize: 'Délka slova',
+		letters: 'Dostupná písmena',
+		getWords: 'Ukaž mi možná slova!',
+		result: 'Slova uvidíš tady',
+		mail: 'Něco nefunguje?'
+	},
+	en: {
+		wordSize: 'Word size',
+		letters: 'Available letters',
+		getWords: 'Show me possible words!',
+		result: 'Words will display here',
+		mail: 'Something is broken?'
 	}
-});
+}
+
+letters = [];
+
+function loadDictionary(lang) {
+	defaultLang = lang;
+	switch(lang) {
+		case "en":
+			path = 'words/en.txt';
+			letters = defaultLetters['en'];
+			setTexts(texts['en']);
+			$('#cs').attr('class', 'btn btn-default btn-lang');
+			$('#en').attr('class', 'btn btn-primary btn-lang');
+			break;
+		case "cs":
+			path = 'words/cs.txt';
+			letters = defaultLetters['cs'];
+			setTexts(texts['cs']);
+			$('#en').attr('class', 'btn btn-default btn-lang');
+			$('#cs').attr('class', 'btn btn-primary btn-lang');
+			break;
+	}
+
+	$.ajax({
+		type: 'get',
+		url: path,
+		success: function(data) {
+			words = data.toLowerCase().split('\n');
+			// debug('words: ' + words);
+			loaded = true;
+			test();
+		}
+	});
+}
+
+function setTexts(texts) {
+	$('#wordSize').attr('placeholder', texts['wordSize']);
+	$('#letters').attr('placeholder', texts['letters']);
+	$('#getWords').val(texts['getWords']);
+	$('#result').html(texts['result']);
+	$('#mail').html(texts['mail']);
+}
 
 function generateLetters() {
 	wordSize = $('#wordSize').val().valueOf();
-
+	var letter = "";
+	switch(defaultLang) {
+		case "en":
+			letter = "letter";
+			break;
+		case "cs":
+			letter = "písmeno";
+			break;
+	}
 	var html = "";
 	for(var i = 0; i < wordSize; i++) {
-		html += "<input type=\"text\" id=\"" + i + "\" class=\"form-control\" placeholder=\"" + (i + 1) +". letter\">";
+		html += "<input type=\"text\" id=\"" + i + "\" class=\"form-control\" placeholder=\"" + (i + 1) +". " + letter + "\" oninput=\"getWords()\">";
 	}
 
 	debug('word size: ' + wordSize);
@@ -42,11 +106,12 @@ function getWord() {
 }
 
 function debug(string) {
-	// console.log('DEBUG: ' + string);
+	console.log('DEBUG: ' + string);
 }
 
 function getWords() {
-	letters = $('#letters').val().toLowerCase().split('');
+	if($('#letters').val() != '')
+		letters = $('#letters').val().toLowerCase().split('');
 	debug('letters: ' + letters);
 
 	word = getWord();
@@ -67,16 +132,28 @@ function getWords() {
 			return regex.exec(element);
 		});
 		matchedWords = matchedWords.filter(function(element) {
-			return element.length <= wordSize;
+			return element.length == wordSize;
 		});
-	}
-	debug('matched words: ' + matchedWords);
 
-	var result = "<p>";
-	matchedWords.forEach(function(word) {
-		result += word + " ";
-	});
-	result += "</p>";
-	// debug('result: ' + result);
-	$('#result').html(result);
+		debug('matched words: ' + matchedWords);
+
+		var result = "";
+		matchedWords.forEach(function(word) {
+			result += word + " ";
+		});
+		// result += "</p>";
+		// debug('result: ' + result);
+		$('#result').html(result);
+	}
 }
+
+loadDictionary(defaultLang);
+
+function test() {
+	// $('#wordSize').val(3);
+	// generateLetters();
+	// $('#1').val('t');
+	// // $('#letters').val('ry');
+	// getWords();
+}
+
